0fc6c73e254ea07d0a1e00106845d43bccdcfa77
tmorozov <radist@desant.biz>
2013-08-02 17:24:11 +0300

    alt & row-fluid

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(str);
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
